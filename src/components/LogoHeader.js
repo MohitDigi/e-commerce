fba11e41cb6d2ceb5065bf63113ac0f83ed3f5b0
Mohit Kalshan <mohit.kalshan@digimantra.com>
2023-07-06 18:28:10 +0530

    update

--- src/components/LogoHeader.js
+++ src/components/LogoHeader.js
@@ -12,44 +12,47 @@ const LogoHeader = () => {
       <div className="logo-header d-flex justify-content-between align-items-center py-4 border-end-0 border-start-0">
         <img src={logo} alt="logo" />
 
+        <form
+          action="/"
+          className="d-flex align-items-center p-3"
+          style={{ border: "2px solid #BCE3C9" }}
+        >
+          {/* All Categories */}
+          <div className="pl-2">
+            <select className="px-3 border-0 bg-white">
+              <option>All Categories</option>
+              <option>Milks and Dairies</option>
+              <option>Wines & Alcohol</option>
+              <option>Clothing & Beauty</option>
+              <option>Pet Foods & Toy</option>
+              <option>Fast food</option>
+              <option>Baking material</option>
+              <option>Vegetables</option>
+              <option>Fresh Seafood</option>
+              <option>Noodles & Rice</option>
+              <option>Ice cream</option>
+            </select>
+          </div>
+          {/* Search Input Box */}
+          <input
+            type="text"
+            placeholder="Search for items..."
+            className="border-0 border-start px-3 mx-3"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </form>
+
         <div className="">
-          <form action="/" className="d-flex align-items-center p-3" style={{border:"2px solid #BCE3C9"}}>
-            {/* All Categories */}
-            <div className="pl-2">
-              <select className="px-3 border-0 bg-white">
-                <option>All Categories</option>
-                <option>Milks and Dairies</option>
-                <option>Wines & Alcohol</option>
-                <option>Clothing & Beauty</option>
-                <option>Pet Foods & Toy</option>
-                <option>Fast food</option>
-                <option>Baking material</option>
-                <option>Vegetables</option>
-                <option>Fresh Seafood</option>
-                <option>Noodles & Rice</option>
-                <option>Ice cream</option>
-              </select>
-            </div>
-            {/* Search Input Box */}
-            <input
-              type="text"
-              placeholder="Search for items..."
-              className="border-0 border-start px-3 mx-3"
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-search"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-            </svg>
-          </form>
-        </div>
-        <div className="">
-          <div className="d-flex justify-content-between mx-3">
+          <div className="d-flex justify-content-between mx-3 px-3">
             <div className="search-location">
               <form action="#">
                 {/* <select className="">
@@ -81,21 +84,31 @@ const LogoHeader = () => {
                 </select> */}
               </form>
             </div>
-            <div className="header-action-icon-2">
+            <div className="header-action-icon-2 position-relative px-3">
               <img className="svgInject" alt="Nest" src={compareIcon} />
               {/* <span className="pro-count blue">3</span> */}
-
+              <span class="position-absolute top-0 start-30 translate-middle badge rounded-pill bg-success">
+                3
+                <span class="visually-hidden">unread messages</span>
+              </span>
               <span className="lable ml-0">Compare</span>
             </div>
-            <div className="header-action-icon-2">
+            <div className="header-action-icon-2 position-relative px-3">
               <img className="svgInject" alt="Nest" src={wishlistIcon} />
               {/* <span className="pro-count blue">6</span> */}
-
+              <span class="position-absolute top-0 start-30 translate-middle badge rounded-pill bg-success">
+                6
+                <span class="visually-hidden">unread messages</span>
+              </span>
               <span className="lable">Wishlist</span>
             </div>
-            <div className="header-action-icon-2">
+            <div className="header-action-icon-2 position-relative px-3">
               <img alt="Nest" src={cartIcon} />
               {/* <span className="pro-count blue">2</span> */}
+              <span class="position-absolute top-0 start-30 translate-middle badge rounded-pill bg-success">
+                2
+                <span class="visually-hidden">unread messages</span>
+              </span>
               <span className="lable">Cart</span>
             </div>
             <div className="header-action-icon-2">
