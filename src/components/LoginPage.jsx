7dc0753fb3d6e29238e9da666615a17dd535f03b
Mohit Kalshan <mohit.kalshan@digimantra.com>
2023-07-17 11:34:58 +0530

    login validation done

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Assets/LoginPage/LoginPage.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const LoginPage = () => {
+  const secCode = "8675";
+  const[user,setUser] = useState("");
+  const[pswd,setPswd] = useState("");
+  const[code,setCode] = useState("");
+
+  const errorMsg = () => toast.error('please fill all the fields !!!');
+  const successMsg = () => toast.success('Successfully logged in !!!')
+
+  const handleSubmit =() => {
+  user==='' || pswd==='' || code !==secCode ? <>{console.log('Please enter value')}{errorMsg()} </> :<> {console.log('Success')}{successMsg()} </>;
+  };
   return (
     <>
       <div className="login-main-container">
@@ -21,10 +35,10 @@ const LoginPage = () => {
           </p>
 
           <div className="login-form-section">
-            <input type="email" name="" placeholder="Username or Email *" required/>
-            <input type="password" name="" placeholder="Your password *" required/>
+            <input type="email" name="" placeholder="Username or Email *" required onChange={(e)=> setUser(e.target.value)}/>
+            <input type="password" name="" placeholder="Your password *" required onChange={(e)=> setPswd(e.target.value)}/>
             <div className="security-section">
-              <input type="text" name="" placeholder="Security code *" required/>
+              <input type="text" name="" placeholder="Security code *" required onChange={(e)=> setCode(e.target.value)}/>
               <div class="security-code">
                 <b class="text-green">8</b>
                 <b class="text-red">6</b>
@@ -40,7 +54,8 @@ const LoginPage = () => {
             </div>
             <a href="">Forget password?</a>
           </div>
-          <button type="submit" className="login-btn">Log in </button>
+          <button type="submit" className="login-btn" onClick={handleSubmit}>Log in </button>
+          <ToastContainer />
         </div>
       </div>
     </>
